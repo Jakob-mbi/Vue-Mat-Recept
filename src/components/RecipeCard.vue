52f0953ed41032687f74d3b76fa84c12041a6717
Jakob-mbi <jakob_mbi@msn.com>
2024-12-17 02:54:43 +0100

    feat recipeCards

---
+++ src/components/RecipeCard.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { defineProps} from 'vue';
+import { Recipe } from './types';
+
+const props = defineProps<{
+    recepie:Recipe;
+}>();
+
+</script>
+
+<template>
+    <div class="bg-white rounded-xl shadow-md relative m-5">
+        <div class="p-4">
+            <div class="mb-6">
+                <div class="text-gray-600 my-2">{{ props.recepie.servings }}</div>
+                <h3 class="text-xl font-bold">{{ props.recepie.name }}</h3>
+            </div>
+
+       
+            <div class="mb-5">
+                <ul v-for="instructions in props.recepie.instructions" :key="props.recepie.id">
+                    <li>{{ instructions }}</li>
+                </ul>
+            </div>
+            
+            
+
+            <div class="border border-gray-100 mb-5"></div>
+
+            <div class="flex flex-col lg:flex-row justify-between mb-4">
+         
+            </div>
+        </div>
+    </div>
+</template>
--- src/components/RecipeList.vue
+++ src/components/RecipeList.vue
@@ -2,10 +2,22 @@
 import recepieData from '../../public/recipes.json';
 import { RecipesData, Recipe} from '@/types';
 import { ref } from 'vue';
+import RecipeCard from './RecipeCard.vue';
 
 const recipes = ref<Recipe[]>(recepieData);
 console.log(recipes.value);
 </script>
 <template>
-    Recipe List
+    <section class="bg-blue-50 px-4 py-10">
+        <div class="container-xl lg:container m-auto">
+            <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+                Browse Recipes
+            </h2>
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <RecipeCard v-for="recepie in recipes.recipes" :key="recepie.id" :recepie="recepie"/>
+
+            </div>
+        </div>
+    </section>
+
 </template>
