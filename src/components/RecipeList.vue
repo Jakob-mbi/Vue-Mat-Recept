418f21d7336ee94d4bac1847ac1f5434bec64969
Jakob-mbi <jakob_mbi@msn.com>
2024-12-19 02:31:37 +0100

    feat update recipeCards

--- src/components/RecipeList.vue
+++ src/components/RecipeList.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
 import recepieData from '../../public/recipes.json';
-import { RecipesData, Recipe} from '@/types';
+import { RecipesData} from '@/types';
 import { ref } from 'vue';
 import RecipeCard from './RecipeCard.vue';
 
-const recipes = ref<Recipe[]>(recepieData);
-console.log(recipes.value);
+defineProps(
+    {
+        limit:{
+            type:Number,
+            default:3
+        },
+        showButton:{
+            type:Boolean,
+            default: false
+        }
+    }
+);
+const recipes = ref<RecipesData>(recepieData);
 </script>
 <template>
     <section class="bg-blue-50 px-4 py-10">
@@ -14,10 +25,17 @@ console.log(recipes.value);
                 Browse Recipes
             </h2>
             <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <RecipeCard v-for="recepie in recipes.recipes" :key="recepie.id" :recepie="recepie"/>
+                <RecipeCard v-for="recepie in recipes.recipes.slice(0,limit || recipes.recipes.length)" :key="recepie.id" :recepie="recepie"/>
 
             </div>
         </div>
+    </section>
+    <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+      <a
+        href="/recipes"
+        class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Recipes</a
+      >
     </section>
 
 </template>
