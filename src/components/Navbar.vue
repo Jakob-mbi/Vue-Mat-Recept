c86dd3f06be8f44c5eeae3d2b0ea569b51ae3460
Jakob-mbi <jakob_mbi@msn.com>
2025-01-02 02:19:42 +0100

    feat active navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
     import Logo from '../assets/img/logo.svg';
-    import {RouterLink} from 'vue-router';
+    import {RouterLink,useRoute} from 'vue-router';
+
+    const isActiveLink = (routePath: string) => {
+        const route = useRoute();
+        return route.path === routePath;
+    }
 </script>
 
 <template>
@@ -21,16 +26,16 @@
             <div class="hidden w-full md:block md:w-auto" id="navbar-default">
                 <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                     <li>
-                    <RouterLink to="/" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                        Home</RouterLink>
+                        <RouterLink to="/" :class="[isActiveLink('/')?'text-green-900':'hover:text-green-700']">
+                            Home</RouterLink>
                     </li>
                     <li>
-                    <RouterLink to="/recipes" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                        Recipes</RouterLink>
+                        <RouterLink to="/recipes" :class="[isActiveLink('/recipes')?'text-green-900':'hover:text-green-700']">
+                            Recipes</RouterLink>
                     </li>
                     <li>
-                    <RouterLink to="/recipes/add" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                        Add Recipes</RouterLink>
+                        <RouterLink to="/recipes/add" :class="[isActiveLink('/recipes/add')?'text-green-900':'hover:text-green-700']">
+                            Add Recipes</RouterLink>
                     </li>  
                 </ul>
             </div>
