f9825a3b9b632dced77fa5fbcac2c36aa72ec326
Jakob-mbi <jakob_mbi@msn.com>
2025-01-30 23:43:00 +0100

    fix favourite

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -88,7 +88,7 @@ const truncatedIngredients = computed(() => {
                 class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm">
                     Read instructions
                 </RouterLink>
-                <i @click="toggleFavourite" :class="['pi',favourite?'pi-star-fill':'pi-star','mr-5' ,'text-xl']" ></i>
+                <i @click="toggleFavourite" :class="['pi',favourite?'pi-star-fill':'pi-star','mr-5' ,'text-xl','mt-2']" ></i>
             </div>
         </div>
     </div>
